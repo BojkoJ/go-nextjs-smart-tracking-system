31c80492666bf6bcafdcc1c4104b782476a4af51
Jan Bojko <honzabojko@seznam.cz>
2026-03-11 22:30:14 +0100

    first hello world k3d deployment ever

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,6 +17,7 @@ terraform {
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
+      version = "2.38.0"
     }
   }
 }
@@ -53,3 +54,105 @@ resource "k3d_cluster" "tracking_system_k3s_cluster" {
   }
 }
 
+// Blok provider nic nevytváří, ale nastavuje
+// tento blok bude dělat nastavení našeho kubernetes (hashicorp/kubernetes nainstalované v terraform/required_providers)
+provider "kubernetes" {
+  // Tímto říkáme: "Všechny příkazy, které se týkají Kubernetes, posílej na tuhle adresu."
+  // Je to ta samá adresa, kterou jsme definovali v bloku k3d_cluster jako host_port v rámci kube_api
+  host = "https://127.0.0.1:6445"
+  // Kubernetes nás dovnitř nepustí jen tak - chce vidět certifikáty - budeme dělat PROPOJOVÁNÍ ZDROJŮ
+  // Uvnitř tohoto bloku budeme dále potřebovat tyto tři položky.
+  // V k3d clusteru jsou tyto údaje uložené ve vnořené struktuře.
+  // V Terraformu se k nim dostaneme přes tuto notaci: TYP_ZDROJE.JMÉNO_ZDROJE.ATRIBUT.
+  // Jsou ale uložené v zakódovaném formátu Base64, aby je provider mohl přečíst musíme je rozbalti pomocí base64decode()
+  client_certificate = k3d_cluster.tracking_system_k3s_cluster.credentials[0].client_certificate
+  client_key = k3d_cluster.tracking_system_k3s_cluster.credentials[0].client_key
+  cluster_ca_certificate = k3d_cluster.tracking_system_k3s_cluster.credentials[0].cluster_ca_certificate
+}
+
+
+// tímto řekneme terraformu: "Použij ten ovladač kubernetes a vytvoř v něm objekt typu deployment"
+// "hello_world" je vnitřní název pro Terraform. Pokud bychom později na tento deployment chtěli odkazovat, použijeme toto jméno.
+resource "kubernetes_deployment" "hello_world" {
+
+  // Tímto explicitně říkáme: "Nesahej na Kubernetes objekty, dokud není zdroj k3d_cluster v pořádku a plně načtený."
+  // Terraform díky tomu nejdříve načte ty správné certifikáty do providera a až pak se pokusí o připojení.
+  depends_on = [k3d_cluster.tracking_system_k3s_cluster]
+
+  metadata {
+    // toto je název přímo uvnitř kubernetes. když později napíšeme v terminálu kubectl get deployments, uvidíme tam toto jméno
+    name = "hello-world-deployment"
+  }
+
+  spec {
+    // v tomto je síla kubernetes, říkáme: "chci aby mi na těch mých dvou agentech běžely vždy 2 kopie této aplikace".
+    // Pokud jedna spadne, kubernetes ji sám restartuje"
+    replicas = 2
+
+    // Toto je jako "lepidlo". Deployment musí vědět, které kontejnery pod něj patří.
+    // říkáme mu: "Sleduj a ovládej všechny kontejnery, které mají na sobě label app="hello-world"
+    selector {
+      match_labels = {
+        app = "hello-world"
+      }
+    }
+
+    // v spec, pod selector se píše template - to je návod, podle kterého kubernetes vyrobí jednotlivé kopie (pods) mojí aplikace
+    template {
+      metadata {
+        // zde se to přesně musí shodovat s tím co je v v selector -> match_labels
+        // Protože tím říkáme: "Tento deployment vyrábí kontejnery s tímto labelem, aby je mohl později poznat a ovládat"
+        labels = {
+          app = "hello-world"
+        }
+      }
+      // teď musíme říct, co v tom Podu skutečně poběží. To se píše také do spec bloku, ale spec bloku uvnitř bloku template
+      spec {
+        // zde definujeme seznam kontejnerů. I když budeme mít jen jeden.
+        // můžeme toto opakovat vícekrát pokud bychom chtěli více kontejnerů
+        container {
+          name = "hello-container"
+          // Jaký obraz se má stáhnout
+          // To je ten software, který distriibuujeme (distrbuovaný systém).
+          // Tento konkrétní obraz od Nginx nám v prohlížeči ukáže, který z našich dvou agentů právě běží
+          image = "nginxdemos/hello"
+          port {
+            // říkáme kubernetes: "Tato aplikace uvnitř kontejneru poslouchá na portu 80"
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+// Nyní potřebujeme service, bez něj by kontejnery sice běžely, ale byly by uzavřené uvnitř clusteru.
+// Nikdy zvenčí by k nim nemohl přistupovat či s nimi komunikovat.
+resource "kubernetes_service" "hello_world_service" {
+  depends_on = [k3d_cluster.tracking_system_k3s_cluster]
+  metadata {
+    // stejně jako u deploymentu, i Service musí mít jméno
+    name = "hello-world-service"
+  }
+
+  // zde definujeme jak má service fungovat
+  spec {
+    selector = {
+        app = "hello-world"
+    }
+
+    // tady definujeme mapování portů uvnitř clusteru
+    port {
+      // na tomto portu bude služba dostupná v rámci sítě Kubernetes
+      port = 80
+      // toto je port, na kterém poslouchá náš kontejner, jak jsme definovali v deploymentu
+      target_port = 80
+    }
+
+    // v k3d cluster jsme definovali host_port = 8080 ... node_filters = ["loadbalancer"]
+    // takže když teď vytvoříme službu typu LoadBalancer, k3d pochopí a automaticky ji připojí na ten připravený port 8080
+    type = "LoadBalancer"
+  }
+}
+
+// po dopsání tohoto terraform init a pak terraform apply
